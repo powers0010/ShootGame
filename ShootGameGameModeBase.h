e9075292fd4508d8eef701b459984566f983365c
powers0010 <31006865+powers0010@users.noreply.github.com>
2017-10-19 23:41:31 +0800

    CreateProgram
    创建项目

---
+++ ShootGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ShootGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTGAME_API AShootGameGameModeBase : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
